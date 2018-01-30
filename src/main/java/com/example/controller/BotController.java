86761c8a464ff7483706c59a817cc13784aa56ec
Aymanov <rihabm@localhost.localdomain>
2018-01-30 12:52:49 +0100

    first commit

---
+++ src/main/java/com/example/controller/BotController.java
@@ -0,0 +1,40 @@
+package com.example.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@LineMessageHandler
+@RestController
+public class BotController {
+	Logger logger = LoggerFactory.getLogger(BotController.class);
+
+	@RequestMapping(value = "/webhook", method = RequestMethod.POST)
+	private @ResponseBody Map<String, Object> webhook(@RequestBody Map<String, Object> obj)
+			throws JSONException, IOException {
+
+
+		Map<String, Object> json = new HashMap<String, Object>();
+
+		
+		logger.info("hay te5dem");
+		return json;
+
+	}
+
+}
---
+++ src/main/java/com/example/demo/HrBotApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@SpringBootApplication
+@LineMessageHandler
+@EnableConfigurationProperties
+@ComponentScan(basePackages = { "com.example.controller" })
+@ComponentScan
+public class HrBotApplication {
+	static Path downloadedContentDir;
+
+	public static void main(String[] args) throws IOException {
+		downloadedContentDir = Files.createTempDirectory("line-bot");
+		SpringApplication.run(HrBotApplication.class, args);
+	}
+
+}
